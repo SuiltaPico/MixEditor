28b4f536f3165fb931c5ee69e43645729017f516
SuiltaPico <256329717@qq.com>
2024-11-26 18:05:39 +0800

    first commit

---
+++ packages/core/src/index.ts
@@ -0,0 +1,8 @@
+import { AnyPlugPluginManager } from "@mauchise/anyplug/dist/index";
+
+class MixEditor {
+  plugins = new AnyPlugPluginManager();
+  constructor() {}
+}
+
+export default MixEditor;
