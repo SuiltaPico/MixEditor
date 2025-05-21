e051d09cbdd3b1ed367972fa5ce86ab46dca357b
SuiltaPico <256329717@qq.com>
2025-05-21 17:48:21 +0800

    新增 temp 实体类型组件，重构实体注册逻辑以支持初始化阶段执行，设置组件映射。

--- packages/core/src/core/ent/temp.ts
+++ packages/core/src/core/ent/temp.ts
@@ -1,13 +1,23 @@
 import { create_ent_registration } from "../../common/ent";
 import { ECSEntInitEvent } from "../../ecs";
+import { TypeCompo } from "../compo";
 
+const ent_type = "core:temp";
+export const temp_ent_TypeCompo = new TypeCompo(ent_type);
 const {
   EntType: TempEntType,
   EntInitPipeId: TempEntInitPipeId,
   register_ent: register_TempEnt,
 } = create_ent_registration({
   namespace: "core",
-  ent_type: "core:temp",
+  ent_type,
+  init_stage_execute: async (event) => {
+    const { ent_id, ex_ctx, params } = event;
+
+    ex_ctx.ecs.set_compos(ent_id, [
+      temp_ent_TypeCompo,
+    ]);
+  },
 });
 
 export { TempEntType, TempEntInitPipeId, register_TempEnt };
